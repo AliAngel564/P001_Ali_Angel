ba1dcd805bf422d9876393fafb78a5612a5e8692
AliAngel564 <jonathan.aliangel@gmail.com>
2025-02-21 08:21:03 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*
+Amerike University
+Author: Ali Angel
+Work #: 1
+Date: 21/02/2025
+Description: This program will print a hello world in the console
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    cout << "Hello World!"; 
+
+    return 0;
+}
